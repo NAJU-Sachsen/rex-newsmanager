8a63e7e46eb28224ddca2ebe8f684d12275d81ab
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-15 23:16:23 +0200

    [WIP] Provide partial backend UI

--- install.sql
+++ install.sql
@@ -1,18 +1,36 @@
 
-create table if not exists naju_news_article (
-	id int(10) unsigned not null auto_increment,
-	title varchar(125) not null,
-	subtitle varchar(125) default '',
-	group_id int(10) unsigned not null,
-	
-	published date default null,
-	updated date default null,
-	status varchar(30) not null,
+create table if not exists naju_blog (
+	blog_id int(10) unsigned not null auto_increment,
+	blog_title varchar(125) not null,
+	blog_group int(10) unsigned not null
 
-	image varchar(75) default '',
-	article int(10) unsigned,
-	intro_text mediumtext default '',
+	primary key (blog_id),
+	foreign key fk_blog_group (blog_title) references naju_local_group(group_id)
+);
 
-	primary key (id),
-	foreign key fk_news_article_id (article) references rex_article(id)
+create table if not exists naju_blog_article (
+	article_id int(10) unsigned not null auto_increment,
+	article_title varchar(255) not null,
+	article_subtitle varchar(255) default '',
+	article_blog int(10) unsigned not null,
+
+	-- further reading
+	article_link int(10) unsigned,
+	article_link_text varchar(255) default '',
+
+	-- image
+	article_image varchar(255) default '',
+
+	-- content
+	article_intro text not null,
+	article_content text not null,
+
+	-- status
+	article_updated date not null,
+	article_published date not null,
+	article_status enum('published', 'archived', 'draft', 'pending') default 'published',
+
+	primary key (article_id),
+	foreign key fk_blog_rex_article (article_link) references rex_article(id),
+	foreign key fk_blog_article (article_blog) references naju_blog(blog_id)
 )
--- pages/list.php
+++ pages/list.php
@@ -1,7 +1,77 @@
 <?php
 
+define('ROWS_PER_PAGE', 25);
+define('PAGER_PARAM', 'offset');
+
 $fragment = new rex_fragment();
+$pager = new rex_pager(ROWS_PER_PAGE, PAGER_PARAM);
+$sql = rex_sql::factory();
 
+// determine the total number of blog articles
+$query = 'SELECT COUNT(*) AS count FROM naju_blog_article';
+$sql->setQuery($query);
+$n_rows = $sql->getArray()[0]['count'];
 
+// prepare paged query
+$query = 'SELECT article_id, article_title, article_updated, article_status, blog_name, article_blog
+          FROM naju_blog_article JOIN naju_blog
+          ON article_blog = blog_id ';
+$query .= 'LIMIT ' . ROWS_PER_PAGE;
+$offset = rex_get(PAGER_PARAM, 'int', 0);
+if ($offset) {
+    $query .= ' OFFSET ' . $offset;
+}
 
+// fetch query result and prepare pagination
+$sql->setQuery($query);
+$res = $sql->getArray();
+$pager->setRowCount($n_rows);
+$pagination = new rex_fragment();
+$pagination->setVar('pager', $pager, false);
+$pagination->setVar('urlprovider', rex_url::currentBackendPage());
+
+// prepare page output
+$content = '';
+$content .= '<table class="table">
+                <thead>
+                    <tr>
+                        <th>Artikel</th>
+                        <th>Blog</th>
+                        <th>Aktualisiert</th>
+                        <th>Status</th>
+                        <th>Anpassen</th>
+                    </tr>
+                </thead>
+                <tbody>';
+
+foreach ($res as $article) {
+    $content .= '<tr>';
+    $content .= '<td>' . rex_escape($article['article_title']) . '</td>';
+    $content .= '<td>' . rex_escape($article['blog_name']) . '</td>';
+    $content .= '<td>' . rex_escape($article['article_updated']) . '</td>';
+    $content .= '<td>';
+    switch ($article['article_status']) {
+        case 'published':
+            $content .= 'online';
+            break;
+        case 'archived':
+            $content .= 'archiviert';
+            break;
+        case 'draft':
+            $content .= 'Entwurf';
+            break;
+        case 'pending':
+            $content .= 'wartet auf Veröffentlichung';
+            break;
+    }
+    $content .= '</td>';
+    $content .= '<td></td>'; // TODO: edit options
+    $content .= '</tr>';
+}
+
+$content .= '</tbody></table>';
+
+// show output
+$content .= $pagination->parse('core/navigations/pagination.php');
+$fragment->setVar('content', $content);
 echo $fragment->parse('core/page/section.php');
