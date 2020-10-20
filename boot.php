61f5287024b850469f928e5ee09c5d83b2c82ae2
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-20 18:35:32 +0200

    Drop number of stored articles upon form submit

--- boot.php
+++ boot.php
@@ -4,3 +4,9 @@
 if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
     rex_cronjob_manager::registerType('naju_article_publish_cronjob');
 }
+
+if (rex::isBackend()) {
+    rex_extension::register('REX_FORM_SAVED', function ($ep) {
+        naju_kvs::invalidate('naju.blogs.*.count');
+    });
+}
