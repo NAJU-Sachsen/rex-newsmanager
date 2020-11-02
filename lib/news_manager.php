ee1ed1ed7395b2cccf979a0ce9a5ce8989bfdfd8
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-11-02 22:37:35 +0100

    Also extract article content in blog news manager

--- lib/news_manager.php
+++ lib/news_manager.php
@@ -7,7 +7,7 @@ class naju_news_manager
         $sql = rex_sql::factory();
         $sql->setTable('naju_blog_article')
             ->setWhere('article_id = :id', ['id' => $article_id])
-            ->select('article_title, article_subtitle, article_link, article_link_text, article_image, article_published, article_updated');
+            ->select('article_title, article_subtitle, article_link, article_link_text, article_image, article_published, article_updated, article_content');
         $article = $sql->getArray();
         return $article ? $article[0] : null;
     }
